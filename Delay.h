5ab53bf5244713c4208513cc698ed355dd94fc14
Afungay <165143972+Afungay@users.noreply.github.com>
2024-04-19 22:32:54 +0800

    Add files via upload

---
+++ Delay.h
@@ -0,0 +1,6 @@
+#ifndef__DELAY_H__
+#define__DELAY_H__
+void Delay(unsigned int xms);	//@12.000MHz
+
+
+#endif
---
+++ MatrixLED.h
@@ -0,0 +1,8 @@
+#ifndef __Matrix_LED_H__
+#define __Matrix_LED_H__
+
+void MatrixLED_ShowColumn(unsigned char Column,Data);
+void MatrixLED_Init();
+
+
+#endif
